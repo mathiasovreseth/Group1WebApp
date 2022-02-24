c6685ac84b99edf4251ebd68d542af5def4df1ba
MicBerg <michal.berg99@gmail.com>
2022-02-24 13:30:09 +0100

    Added css style to outer container of the hero section

--- redcross-aalesund/src/components/heroSection/heroSection.tsx
+++ redcross-aalesund/src/components/heroSection/heroSection.tsx
@@ -2,12 +2,24 @@ import React from "react";
 import styled from "styled-components";
 
 const OuterContainer = styled.div`
+  min-width: 100rem;
+  height: 20vw;
+  border: 1px solid black;
+  background-size: 100%;
+  min-height: 42rem;
+  max-height: 60rem;
+  position: relative;
+  background-color: white;
+`;
+
+const Button = styled.button`
   color: red;
-  font-size: 3.6rem;
 `;
 
 function HeroSection() {
-  return <OuterContainer id={"hero-section"}>Harry Maguire</OuterContainer>;
+    return <OuterContainer id={"hero-section"}>Harry Maguire
+        <Button>Book your training</Button>
+    </OuterContainer>;
 }
 
 export default HeroSection;
