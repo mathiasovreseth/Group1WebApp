166c5d6a2fec3f79ee304db76ca4e89236f3cb4d
Mathias Jørgensen <mathijo@stud.ntnu.no>
2022-02-24 13:39:04 +0100

    added font

--- redcross-aalesund/src/styles/globalStyles.tsx
+++ redcross-aalesund/src/styles/globalStyles.tsx
@@ -1,12 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     font-size: 62.5%;
     background: teal;
     font-family: Open-Sans, Helvetica, Sans-Serif;
+    font-family: myriad-pro, sans-serif;
+    font-weight: 700;
+    font-style: normal;
   }
 `;
 
