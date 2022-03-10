42bdd6608a2ac172e98d242a9841a8ede10ca248
Mathias Jørgensen <mathijo@stud.ntnu.no>
2022-03-10 14:28:30 +0100

    added courses and flexboxes

--- redcross-aalesund/src/App.tsx
+++ redcross-aalesund/src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 import HeroSection from "./components/heroSection/heroSection";
 import GlobalStyles from "./styles/globalStyles";
 import Header from "./components/header/header";
+import Courses from "./components/courses/courses";
 
 export default function App() {
-    return (
-        <>
-            <GlobalStyles/>
-            <Header/>
-            <HeroSection/>
-        </>
-    );
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+      <HeroSection />
+      <Courses />
+    </>
+  );
 }
-
