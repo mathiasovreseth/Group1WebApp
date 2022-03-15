0be92c74a5a095d8c0a4ba36206e4ad34b1bf904
SjurG <sjurgustavsen@gmail.com>
2022-03-15 09:22:02 +0100

    comments responsive

--- redcross-aalesund/src/components/trustedReview/trustedReview.tsx
+++ redcross-aalesund/src/components/trustedReview/trustedReview.tsx
@@ -3,19 +3,28 @@ import styled from "styled-components";
 import image from "../../assets/baby.jpg"
 
 const H1 = styled.h1`
-  color: red;
+  color: #D52D27;
   font-size: 4.8rem;
   font-weight: normal;
-  margin: 10% 8rem;
+  margin: 2rem;
+  @media screen and (max-width: 800px){
+    font-size: 3rem;
+  }
+  text-align:center;
 `;
 
 const TrustedComment = styled.section`
     display: grid; 
-    grid-template-columns: 230px auto; 
-    grid-template-rows: 230px auto; 
-    gap: 0px 0px; 
-    box-shadow: 0 0 3rem 0 rgba(90, 90, 90, 0.2);
+    padding-left: 5%;
+    padding-right: 5%;
+    grid-template-columns: 12rem auto; 
+    grid-template-rows: 12rem auto; 
+    // box-shadow: 0 0 1rem 0 rgba(90, 90, 90, 0.2);
     grid-gap: 50px;
+    @media screen and (max-width: 800px){
+        grid-template-columns: 8rem auto; 
+        grid-template-rows: 8rem auto; 
+    }
 `;
 
 const CommentPicture = styled.div`
@@ -23,15 +32,23 @@ const CommentPicture = styled.div`
 `;
 
 const Comment = styled.div`
-    font-size: 4rem;
+    font-size: 3rem;
     align-self:center;
+    @media screen and (max-width: 800px){
+        font-size: 2rem; 
+    }
 `;
 
 const Image = styled.img`
     border-radius: 50%;
     object-fit: cover;
-    width:230px;
-    height:230px;
+    width:12rem;
+    height:12rem;
+    align-self:center;
+    @media screen and (max-width: 800px){
+        width:8rem;
+        height:8rem;
+    }
 `;
 
 
@@ -45,7 +62,7 @@ function TrustedReviews() {
                 <CommentPicture> <Image src={image} alt="Profile picture" /> </CommentPicture>
                 <Comment>
                     <h2>Sjur Gustavsen</h2>
-                    <p>Very good! yes!</p>
+                    <p>Quisque pretium nulla vitae velit euismod, quis volutpat justo vehicula. Etiam cursus nulla ac hendrerit sagittis. Fusce molestie ligula pellentesque eleifend dignissim. Morbi rhoncus, lorem id vehicula dignissim, nulla risus vehicula nunc, at tempus dolor massa ut orci. Quisque sollicitudin nisi eu nulla elemen</p>
                     </Comment>
             </TrustedComment>
 
