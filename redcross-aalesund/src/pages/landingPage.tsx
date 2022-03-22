2221273135c9ed5cd3513367caf0f5f0c7e37a9d
Mathias Jørgensen <mathijo@stud.ntnu.no>
2022-03-22 12:41:47 +0100

    added responsive css too Courses

--- redcross-aalesund/src/pages/landingPage.tsx
+++ redcross-aalesund/src/pages/landingPage.tsx
@@ -3,12 +3,14 @@ import GlobalStyles from "../styles/globalStyles";
 import Header from "../components/header/header";
 import HeroSection from "../components/heroSection/heroSection";
 import Footer from "../components/footer/footer";
+import Courses from "../components/courses/courses";
 
 const LandingPage = () => {
     return (
        <>
         <Header/>
         <HeroSection/>
+        <Courses/>
         <Footer/>
        </>
     );
