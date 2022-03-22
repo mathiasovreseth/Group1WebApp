f1b4a78aa5007f066de2392a997896f34d4001a8
MicBerg <michal.berg99@gmail.com>
2022-03-22 12:43:17 +0100

    Added OurWork and TrustedReviews to the landing page

--- redcross-aalesund/src/pages/landingPage.tsx
+++ redcross-aalesund/src/pages/landingPage.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import GlobalStyles from "../styles/globalStyles";
 import Header from "../components/header/header";
 import HeroSection from "../components/heroSection/heroSection";
+import OurWork from "../components/ourWork/ourWork";
 import Footer from "../components/footer/footer";
+import TrustedReviews from "../components/trustedReview/trustedReview";
+
 
 const LandingPage = () => {
     return (
-       <>
-        <Header/>
-        <HeroSection/>
-        <Footer/>
-       </>
+        <>
+            <Header/>
+            <HeroSection/>
+            <OurWork/>
+            <TrustedReviews/>
+            <Footer/>
+        </>
     );
 }
 export default LandingPage;
