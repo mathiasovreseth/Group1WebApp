b820c752e4a42aac3cf930d9d1bbc2958eaae22f
SjurG <sjurgustavsen@gmail.com>
2022-03-31 13:22:47 +0200

    Order controller, repository and service

--- backend/src/main/java/no/ntnu/group1/webApp/controller/OrderController.java
+++ backend/src/main/java/no/ntnu/group1/webApp/controller/OrderController.java
@@ -1,8 +1,15 @@
 package no.ntnu.group1.webApp.controller;
 
+import no.ntnu.group1.webApp.models.Order;
 import no.ntnu.group1.webApp.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/orders")
@@ -11,5 +18,15 @@ public class OrderController {
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Order>> getAllOrders() {
+        return  ResponseEntity.ok(orderService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Order>> getOrderById(@PathParam("id") Long id) {
+        return ResponseEntity.ok(orderService.findById(id));
     }
 }
--- backend/src/main/java/no/ntnu/group1/webApp/service/OrderService.java
+++ backend/src/main/java/no/ntnu/group1/webApp/service/OrderService.java
@@ -1,7 +1,12 @@
 package no.ntnu.group1.webApp.service;
 
+import no.ntnu.group1.webApp.models.Order;
+import no.ntnu.group1.webApp.models.User;
 import no.ntnu.group1.webApp.repositories.OrderRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
@@ -10,4 +15,13 @@ public class OrderService {
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
     }
+
+    public List<Order> getAll() {
+        return (List<Order>) orderRepository.findAll();
+    }
+
+    public Optional<Order> findById(Long id) {
+        return orderRepository.findById(id);
+    }
+
 }
