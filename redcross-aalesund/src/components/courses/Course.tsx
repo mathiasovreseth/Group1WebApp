87c2fdd7a5a8aa912e4fd5d66715a615d5d1a2de
SjurG <sjurgustavsen@gmail.com>
2022-04-21 17:45:29 +0200

    adaptive cards and card component

---
+++ redcross-aalesund/src/components/courses/Course.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { FaSquare } from "react-icons/fa";
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
+const Section = styled.div`
+  min-height: 25rem;
+  min-width: 30rem;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin: 5rem 0 1rem 0;
+  border-radius: ${props => `${props.theme.borderRadius}`};
+  background: #ededed;
+  justify-content: center;
+  align-items: center;
+  transform: scale(1);
+  transition: all 0.4s;
+   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  @media  (max-width: ${props => `${props.theme.breakPoints.tabletLandScape}`}){
+    margin: 2rem 0 2rem 0;
+  }
+  &:hover,
+  &:focus {
+    transform: scale(1.02);
+  }
+  
+`;
+
+const Box = styled.h1`
+  height: 5rem;
+  width: 30rem;
+  position: absolute;
+  top: 0%;
+  background-color: #f16670;
+  text-align: center;
+  font-size: ${props => `${props.theme.fontSizes.large}`};
+`;
+
+const SelectButton = styled.button`
+  background-color:#f16670; 
+  border: none;
+  color: white;
+  padding: 1rem 2.5rem;
+  text-align: center;
+  text-decoration: none;
+  font-size: 1.2rem;
+  display: inline-block;
+  margin: 4rem 0 1rem 0;
+  cursor: pointer;
+  border-radius: ${props => `${props.theme.borderRadius}`};
+`;
+
+const BulletPoints = styled.ul`
+  list-style-type: square;
+  text-align: left;
+  padding-top:20%;
+  max-width: 25rem;
+  font-size: ${props => `${props.theme.fontSizes.xSmall}`};
+  margin:1rem 0 1rem 0;
+`;
+
+const LI = styled.li`
+  font-size: ${props => `${props.theme.fontSizes.xSmall}`};
+  margin: 0.3rem 0 0 0;
+`;
+
+
+
+export function Course(props: { title: any; info: any; hasButton: boolean; }) {
+    const title = props.title
+    const info = props.info
+    const hasButton = props.hasButton    
+
+    function createButton() {
+      if(hasButton) {
+        return(
+          <Link to='/product_page'>
+            <SelectButton>Select</SelectButton>
+          </Link>);
+      }
+    }
+
+    
+
+    return (
+      <>
+        <Section>
+            <Box>{title}</Box>
+            <BulletPoints>
+            {info.map((text: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined) =>
+            <LI><FaSquare style={{marginRight: ".8rem"}}/>{text}</LI>)
+            }
+            </BulletPoints>
+            {createButton()}
+  
+        </Section>
+        </>
+    );
+}
+
--- redcross-aalesund/src/components/courses/CoursesNoButton.tsx
+++ redcross-aalesund/src/components/courses/CoursesNoButton.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 import { FaSquare } from "react-icons/fa";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
+import { Class } from "leaflet";
+import {Course} from "./Course";
 
 const H1 = styled.h1`
   color: #d52d27;
@@ -13,6 +15,8 @@ const H1 = styled.h1`
   }
   text-align: center;
 `;
+
+
 const CoursesContainer = styled.div`
   width: 100%;
   display: flex;
@@ -27,95 +31,25 @@ const CoursesContainer = styled.div`
   }
 `;
 
-const Section = styled.div`
-  min-height: 25rem;
-  min-width: 30rem;
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  margin: 5rem 0 1rem 0;
-  border-radius: ${props => `${props.theme.borderRadius}`};
-  background: #ededed;
-  justify-content: center;
-  align-items: center;
-   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  @media  (max-width: ${props => `${props.theme.breakPoints.tabletLandScape}`}){
-    margin: 2rem 0 2rem 0;
-  }
-`;
 
-const Box = styled.h1`
-  height: 5rem;
-  width: 30rem;
-  position: absolute;
-  top: 0%;
-  background-color: #f16670;
-  text-align: center;
-  font-size: ${props => `${props.theme.fontSizes.large}`};
-`;
 
-const SelectButton = styled.button`
-  background-color:#f16670; 
-  border: none;
-  color: white;
-  padding: 1rem 2.5rem;
-  text-align: center;
-  text-decoration: none;
-  font-size: 1.2rem;
-  display: inline-block;
-  margin: 4rem 0 1rem 0;
-  cursor: pointer;
-  border-radius: ${props => `${props.theme.borderRadius}`};
-`;
-
-const BulletPoints = styled.ul`
-  list-style-type: square;
-  text-align: left;
-  padding-top:20%;
-  max-width: 25rem;
-  font-size: ${props => `${props.theme.fontSizes.xSmall}`};
-  margin:1rem 0 4rem 0;
-`;
-
-const LI = styled.li`
-  font-size: ${props => `${props.theme.fontSizes.xSmall}`};
-  margin: 0.3rem 0 0 0;
-`;
+const CardLeft = ["Info1", "info2", "Info3"];
+const CardMiddle = ["Info1", "info2", "Info3"];
+const CardRight= ["Info1", "info2", "Info3"];
 
 
 function CoursesNoButton() {
-    return (
-      <div>
-        <H1>Our Courses</H1>
-        <CoursesContainer>
-            <Section>
-                <Box>1 Day Course</Box>
-                <BulletPoints>
-                  <LI><FaSquare style={{marginRight: ".8rem"}}/> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a iaculis nulla.</LI>
-                  <LI><FaSquare style={{marginRight: ".8rem"}}/> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a iaculis nulla.</LI>
-                  <LI><FaSquare style={{marginRight: ".8rem"}}/> Something else random stuff that doesnt mattter but is nice to have!</LI>
-                </BulletPoints>
-                
-            </Section>
-            <Section>
-                <Box>2 Day Course</Box>
-                <BulletPoints>
-                <LI><FaSquare style={{marginRight: ".8rem"}}/> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a iaculis nulla.</LI>
-                <LI><FaSquare style={{marginRight: ".8rem"}}/> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a iaculis nulla.</LI>
-                <LI><FaSquare style={{marginRight: ".8rem"}}/> Something else random stuff that doesnt mattter but is nice to have!</LI>
-                </BulletPoints>
-            </Section>
-            <Section>
-                <Box>Short Conciliation</Box>
-                <BulletPoints>
-                <LI><FaSquare style={{marginRight: ".8rem"}}/> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a iaculis nulla.</LI>
-                <LI><FaSquare style={{marginRight: ".8rem"}}/> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a iaculis nulla.</LI>
-                <LI><FaSquare style={{marginRight: ".8rem"}}/> Something else random stuff that doesnt mattter but is nice to have!</LI>
-                </BulletPoints>         
-                </Section>
-        </CoursesContainer>
-        </div>
-    );
+  return(
+    <>
+    <H1>Our Courses</H1>
+    <CoursesContainer>
+        <Course title="Left" info={CardLeft} hasButton={false}/> 
+        <Course title="Middle" info={CardMiddle} hasButton={false}/>
+        <Course title="Right" info={CardRight} hasButton={false}/>
+    </CoursesContainer>
+    
+    </>
+  )
 }
 
 export default CoursesNoButton;
