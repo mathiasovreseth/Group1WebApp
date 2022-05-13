da30588a00fa8a1110ee0a7c87fbcdd70e70336f
Michal Berg <michalb@stud.ntnu.no>
2022-05-13 13:16:56 +0200

    Fixed constructor in DummyInitializer class.

--- backend/src/main/java/no/ntnu/group1/webApp/DummyDataInitializer.java
+++ backend/src/main/java/no/ntnu/group1/webApp/DummyDataInitializer.java
@@ -37,8 +37,8 @@ public class DummyDataInitializer implements ApplicationListener<ApplicationRead
         }
 
         System.out.println("Importing test data...");
-        User sjur = new User("Sjur","sjur@mail.com","superpass", "ADMIN");
-        User notAdmin = new User("Bob", "bob@mail.com", "passpass", "ADMIN");
+        User sjur = new User("Sjur","sjur@mail.com","superpass");
+        User notAdmin = new User("Bob", "bob@mail.com", "passpass");
         userRepository.save(sjur);
         userRepository.save(notAdmin);
 
