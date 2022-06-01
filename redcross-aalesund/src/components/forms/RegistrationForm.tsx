f706443e39fff3ca4b398aa092e2ce35445ce022
MicBerg <michal.berg99@gmail.com>
2022-06-01 11:57:42 +0200

    Added additional styling for login and registration forms.

--- redcross-aalesund/src/components/forms/RegistrationForm.tsx
+++ redcross-aalesund/src/components/forms/RegistrationForm.tsx
@@ -6,11 +6,11 @@ import {
 } from "../../styles/CommonStyles";
 import styled from "styled-components";
 import {isValidEmail, isValidPassword, isValidUsername} from '../../utils/FormValidation';
-import { ToastContainer, toast } from 'react-toastify';
+import {ToastContainer, toast} from 'react-toastify';
 import {Link, useNavigate} from "react-router-dom";
 import {useState} from "react";
 import 'react-toastify/dist/ReactToastify.css';
-import { useAuth } from '../../auth/Auth';
+import {useAuth} from '../../auth/Auth';
 import {UserRegistrationFormValues} from "../../models/UserModel";
 
 const Button = styled.button`
@@ -20,7 +20,7 @@ const Button = styled.button`
   font-size: ${props => `${props.theme.fontSizes.large}`};
   padding: 1rem 0;
   background-color: ${props => `${props.theme.palette.primary.accentColor}`};
-  box-shadow: 0 0 5rem 0 rgba(90, 90, 90);
+  box-shadow: 0 0 2rem 0 rgba(90, 90, 90);
   color: white;
   cursor: pointer;
 `;
@@ -33,7 +33,7 @@ const Label = styled.label`
 const FormContainer = styled(FlexColumnContainer)`
   background-color: ${props => `${props.theme.palette.primary.background}`};
   border-radius: ${props => `${props.theme.borderRadius}`};
-  box-shadow: 0 0 2rem 0 rgba(90, 90, 90);
+  box-shadow: 0 0 1.5rem 0 rgba(90, 90, 90);
   padding: 0 4.4rem 6.6rem 4.4rem;
   width: 40rem;
 `;
@@ -50,7 +50,7 @@ const OuterContainer = styled(FlexColumnContainer)`
   width: 100vw;
   height: 90vh;
   position: relative;
-  
+
 `
 
 interface FormValues {
@@ -86,7 +86,7 @@ function RegistrationForm() {
                 autoClose: 1000,
                 hideProgressBar: false,
                 closeOnClick: true,
-                bodyStyle:{fontSize: "3.2rem"},
+                bodyStyle: {fontSize: "3.2rem"},
                 pauseOnHover: true,
                 draggable: true,
                 progress: undefined,
@@ -98,13 +98,14 @@ function RegistrationForm() {
         });
 
     }
+
     function validateForm() {
         let isValid = true;
         if (name.length === 0) {
             setNameErr('Name is required');
             isValid = false;
 
-        } else if(!isValidUsername(name)) {
+        } else if (!isValidUsername(name)) {
             setNameErr("name must be lowercase and contain only letters");
         }
         if (email.length === 0) {
@@ -115,19 +116,20 @@ function RegistrationForm() {
             setEmailErr('Invalid email address');
             isValid = false;
         }
-        if(password.length === 0) {
+        if (password.length === 0) {
             setPassErr('Password is required');
             isValid = false;
 
-        } else if(!isValidPassword(password)) {
+        } else if (!isValidPassword(password)) {
             setPassErr('Password must be 6 characters or more');
             isValid = false;
         }
         return isValid;
     }
+
     return (
-        <form  onSubmit={(e)=> {
-            if(validateForm()) {
+        <form onSubmit={(e) => {
+            if (validateForm()) {
                 setEmailErr('');
                 setNameErr('');
                 setPassErr('');
@@ -153,23 +155,25 @@ function RegistrationForm() {
                 <FormContainer>
                     <LargeText style={{marginTop: "2rem", marginBottom: "4rem"}}>Register</LargeText>
                     <Label>Name</Label>
-                    <Input onChange={(e)=> setName(e.target.value)}  type="text" name="name"/>
+                    <Input onChange={(e) => setName(e.target.value)} type="text" name="name"/>
                     {emailErr && <XSmallText style={{color: "red"}}>{nameErr}</XSmallText>}
                     <Label>E-mail</Label>
-                    <Input onChange={(e)=> setEmail(e.target.value)}  type="email" name="email"/>
+                    <Input onChange={(e) => setEmail(e.target.value)} type="email" name="email"/>
                     {emailErr && <XSmallText style={{color: "red"}}>{emailErr}</XSmallText>}
                     <Label>Password</Label>
-                    <Input onChange={(e)=> setPassword(e.target.value)} type="password" name="password"/>
+                    <Input onChange={(e) => setPassword(e.target.value)} type="password" name="password"/>
                     {passErr && <XSmallText style={{color: "red"}}>{passErr}</XSmallText>}
                     <Button type="submit" disabled={isSubmitting}>
-                        {isSubmitting  ? 'Submitting': 'Submit'}
+                        {isSubmitting ? 'Submitting' : 'Submit'}
                     </Button>
 
-                    <SmallText style={{ marginTop: "2rem", display:"flex", flexWrap: "wrap"}}>
-                        <SmallText style={{ opacity: 0.7}}>By creating an account yo agree to our </SmallText> <Link to={"/privacy-policy"} > <SmallText>Privacy Policy</SmallText></Link>
-                        <SmallText style={{ opacity: 0.7, margin: "0 .2rem"}}>and </SmallText> <Link to={"/terms-of-service"} > <SmallText>Terms of Service</SmallText></Link>
+                    <SmallText style={{marginTop: "2rem", display: "flex", flexWrap: "wrap"}}>
+                        <SmallText style={{opacity: 0.7}}>By creating an account yo agree to our </SmallText> <Link
+                        to={"/privacy-policy"}> <SmallText>Privacy Policy</SmallText></Link>
+                        <SmallText style={{opacity: 0.7, margin: "0 .2rem"}}>and </SmallText> <Link
+                        to={"/terms-of-service"}> <SmallText>Terms of Service</SmallText></Link>
                     </SmallText>
-                    <SmallText style={{ display: "flex", marginTop: "2rem"}}>
+                    <SmallText style={{display: "flex", marginTop: "2rem"}}>
                         <SmallText style={{opacity: 0.7, marginRight: "0.4rem"}}>
                             Already have an account?
                         </SmallText>
@@ -184,6 +188,7 @@ function RegistrationForm() {
         </form>
     )
 }
+
 // interface FormProps {
 //     initialName?: string;
 //     initialEmail?: string;
