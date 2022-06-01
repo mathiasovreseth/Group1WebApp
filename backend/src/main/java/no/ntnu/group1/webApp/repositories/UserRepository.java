878613b1a5edfac503bb6d23be5361d36457a6b5
MicBerg <michal.berg99@gmail.com>
2022-06-01 12:24:48 +0200

    Removed unecessary methods from UserService and UserRepository classes.

--- backend/src/main/java/no/ntnu/group1/webApp/repositories/UserRepository.java
+++ backend/src/main/java/no/ntnu/group1/webApp/repositories/UserRepository.java
@@ -6,13 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    Optional<User> findByName(String name);
 
-    Optional<User> findById(Long id);
+  Optional<User> findById(Long id);
 
-    Optional<User> findByEmail(String email);
-
-    Optional<User> findByToken(String token);
-
+  Optional<User> findByEmail(String email);
 
 }
