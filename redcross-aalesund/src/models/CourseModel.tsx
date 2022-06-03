6f120aaab41f695d3e3f273b8e363f54d8e47cd0
Mathias Jørgensen <mathijo@stud.ntnu.no>
2022-06-03 15:14:44 +0200

    Added commentsection

--- redcross-aalesund/src/models/CourseModel.tsx
+++ redcross-aalesund/src/models/CourseModel.tsx
@@ -2,5 +2,8 @@ export interface getCoursesApiResponse{
     id: number;
     description: string;
     title: string;
-    reviews: Array<any>;
+    reviews: Array<[
+      id: number,
+      comment: string]
+    >;
   }
