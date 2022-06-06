83bdd8aa33f6721ea4958f9cc17bc3cc1688614b
MicBerg <michal.berg99@gmail.com>
2022-06-06 12:58:41 +0200

    Added additional comments to the interfaces in the repository folder.

--- backend/src/main/java/no/ntnu/group1/webApp/repositories/ReviewRepository.java
+++ backend/src/main/java/no/ntnu/group1/webApp/repositories/ReviewRepository.java
@@ -8,11 +8,16 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * The interface Review repository.
+ * The Review repository interface.
  */
 public interface ReviewRepository extends CrudRepository<Review, Long> {
     Optional<Review> findById(Long id);
 
+    /**
+     * Gets comment and user.
+     *
+     * @return the comment and user
+     */
     @Query(value = "SELECT reviews.id, users.name, comment from reviews inner join products_reviews pr on reviews.id = pr.reviews_id inner join users_reviews ur on reviews.id = ur.reviews_id inner join users on ur.user_id = users.id",
             nativeQuery = true)
     List<Object[]> getCommentAndUser();
