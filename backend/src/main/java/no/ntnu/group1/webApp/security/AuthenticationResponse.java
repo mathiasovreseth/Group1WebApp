c7b478c86963bba0cadf8a8abb79b5e1cc76b276
SjurG <sjurgustavsen@gmail.com>
2022-06-06 17:56:35 +0200

    cleanup

--- backend/src/main/java/no/ntnu/group1/webApp/security/AuthenticationResponse.java
+++ backend/src/main/java/no/ntnu/group1/webApp/security/AuthenticationResponse.java
@@ -14,13 +14,4 @@ public class AuthenticationResponse {
     public AuthenticationResponse(String jwt) {
         this.jwt = jwt;
     }
-
-    /**
-     * Gets the JSON web token
-     *
-     * @return the JSON web token
-     */
-    public String getJwt() {
-        return jwt;
-    }
 }
