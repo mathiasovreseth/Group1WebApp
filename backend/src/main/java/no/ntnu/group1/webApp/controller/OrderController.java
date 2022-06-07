378963f969fe16560ea724a92c5a1e5e34c847da
MicBerg <michal.berg99@gmail.com>
2022-06-07 06:45:43 +0200

    Removed unused import.

--- backend/src/main/java/no/ntnu/group1/webApp/controller/OrderController.java
+++ backend/src/main/java/no/ntnu/group1/webApp/controller/OrderController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -110,7 +109,6 @@ public class OrderController {
                 return new ResponseEntity("Failed to process order", HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } catch (JSONException e) {
-            e.printStackTrace();
             return new ResponseEntity("Field(s) missing or null in request", HttpStatus.BAD_REQUEST);
         }
 
